8d7dcaf4ba42e1e519ace21dd4af0c80222d173c
WT-MM <wesley.maa@gmail.com>
2024-02-19 00:59:06 -0500

    clean up

--- autokhan/README.md
+++ autokhan/README.md
@@ -1,3 +1,7 @@
 # Automatic Point Collector
 
 Might add some LLM support to tackle things like unit tests, but for now it's gonna be random. The effort to payoff ratio of making this a smart system is kinda too high right now. 
+
+Currently at 10s per cycle (two options per question), so 6 cycles per minute. At a minimum of 100pts per cycle, that's 600pts per minute.
+
+Then to start from scratch would take 10,000,000/600 = 16,666.67 minutes = 277.78 hours = 11.57 days.
--- autokhan/main.py
+++ autokhan/main.py
@@ -22,28 +22,18 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), opti
 # Function to navigate to a specific exercise or page
 def navigate_to_exercise(url):
     driver.get(url)
-    # Add any additional logic needed for interaction with the exercise
 
 def clickTextandCheck(letter):
-    WebDriverWait(driver, 0.2).until(EC.element_to_be_clickable((By.XPATH, f"//div[contains(text(), '{letter}')]/ancestor::li//button"))).click()
-    #answer = driver.find_element(By.XPATH, f"//div[contains(text(), '{letter}')]/ancestor::li//button")
-    #answer.click()
-    WebDriverWait(driver, 0.2).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[data-test-id="exercise-check-answer"]'))).click()
-    #check = driver.find_element(By.CSS_SELECTOR, 'button[data-test-id="exercise-check-answer"]')
-    #check.click()
-    #time.sleep(0.5)
     try:
+        WebDriverWait(driver, 0.2).until(EC.element_to_be_clickable((By.XPATH, f"//div[contains(text(), '{letter}')]/ancestor::li//button"))).click()
+        WebDriverWait(driver, 0.2).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[data-test-id="exercise-check-answer"]'))).click()
         WebDriverWait(driver, 0.5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[data-test-id="exercise-next-question"]'))).click()
-        #nextQuestion = driver.find_element(By.CSS_SELECTOR, 'button[data-test-id="exercise-next-question"]')
-        #nextQuestion.click()
         return True
     except:
         return False
     
 def clickLetsGo():
     WebDriverWait(driver, 3).until(EC.element_to_be_clickable((By.XPATH, "//button[.//span[text()='Let’s go']]"))).click()
-    #LetsGo = driver.find_element(By.XPATH, "//button[.//span[text()='Let’s go']]")
-    #LetsGo.click()
 
 
 def run_exercise():
@@ -57,34 +47,17 @@ def run_exercise():
             if(clickTextandCheck(letter)):
                 break
         try:
-            WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//button[.//span[contains(text(), 'Try again')]]"))).click()
-            #try_again_button = driver.find_element(By.XPATH, "//button[.//span[contains(text(), 'Try again')]]")
-            #try_again_button.click()
-            #time.sleep(1)
+            WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, "//button[.//span[contains(text(), 'Try again')]]"))).click()
             clickLetsGo()
-            #time.sleep(1)
         except:
             continue
-        
 
 
-# URL of the specific exercise or page you want to access
 specific_exercise_url = "https://www.khanacademy.org/humanities/grammar/punctuation-the-colon-semicolon-and-more/introduction-to-semicolons/e/introduction-to-semicolons"
-
-# Perform login
-#login_khan_academy(EMAIL, PASSWORD)
 
 #Await user login
 driver.get("https://www.khanacademy.org/login")
 input("Please login to Khan Academy and press Enter to continue...")
-# Navigate to the specific exercise
-
 #driver.implicitly_wait(10)
 navigate_to_exercise(specific_exercise_url)
-#time.sleep(5)
 run_exercise()
-
-# Add any additional code for interaction or waiting here
-
-# Optionally, close the browser when done
-# driver.quit()
